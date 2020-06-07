61878aba9ba52d193783eebf372ec15e62569154
christianbailon <xtian.bailon@gmail.com>
2020-06-08 05:23:43 +0800

    React environment setup using webpack, babel, webpack server and sass

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './src/styles/style.scss';
+
+function App() {
+    return (
+        <div>
+            <h1>Hi, Xtian</h1>
+        </div>
+    )
+   
+}
+
+export default App
