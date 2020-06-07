7814b898927a134bda73a237163c00dc2384e286
christianbailon <xtian.bailon@gmail.com>
2020-06-08 06:27:34 +0800

    using React.Fragment

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './src/styles/style.scss';
+import Child from './src/components/Child';
 
 function App() {
+    //old approach
+    // return (
+    //     <div>
+    //         <Child />
+    //     </div>
+    // )
+
+    //using React.Fragment
+    // return (
+    //     <React.Fragment>
+    //         <Child />
+    //     </React.Fragment>
+    // )
+
+    //using React.Fragment shorthand notation
+    //using React.Fragment have effects on CSS, CSS may not find tags or components in your app
+    //using React.Fragment simplifies the DOM tree and nested tags in your html
     return (
-        <div>
-            <h1>Hi, Xtian</h1>
-        </div>
+        <>
+            <Child />
+        </>
     )
-   
 }
 
 export default App
