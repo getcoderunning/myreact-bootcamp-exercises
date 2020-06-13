f1728375d36b067883bf74f61c60d074d4093c35
christianbailon <xtian.bailon@gmail.com>
2020-06-14 04:27:12 +0800

    implementing callback functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-import App from "../App";
+// import App from "../App";
 
-ReactDOM.render(<App />, document.getElementById("root"))
+// ReactDOM.render(<App />, document.getElementById("root"))
+document.getElementById("button").addEventListener("click", function(event) {
+    console.log(event)
+})
+
+document.getElementById("input").addEventListener("input", function() {
+    console.log("Input changed!")
+})
+
+document.getElementById("box").addEventListener("mouseover", function() {
+    console.log("Hovered in box!")
+})
