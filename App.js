36a090dfa685d8f0c2770f4e3d91b8486d413d56
christianbailon <xtian.bailon@gmail.com>
2020-06-19 05:36:49 +0800

    Implementing useState() example2

--- App.js
+++ App.js
@@ -18,16 +18,57 @@ import React, {useState} from 'react';
 //   }
 // }
 
+// ANCHOR Using useState() example 1
+// function App() {
+// using as object
+// const value = useState('Yes');
+// using arraydestructing
+//   const [answer] = useState('Yes');
+//   console.log(answer);
+
+//   return (
+//     <div>
+//       <h1>Is state important to know? {answer} </h1>
+//     </div>
+//   );
+// }
+
+// ANCHOR Using useState() example 2
+// using class for state
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       count: 0,
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.count}</h1>
+//         <button>Change!</button>
+//       </div>
+//     );
+//   }
+// }
+
+// using function for state
 function App() {
-  // using as object
-  // const value = useState('Yes');
-  // using arraydestructing
-  const [answer] = useState('Yes');
-  console.log(answer);
+  const [count, setCount] = useState(0);
+
+  function increment(){
+    setCount(prevCount => prevCount + 1);
+  }
+  function decrement(){
+    setCount(prevCount => prevCount - 1);
+  }
 
   return (
     <div>
-      <h1>Is state important to know? {answer} </h1>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
