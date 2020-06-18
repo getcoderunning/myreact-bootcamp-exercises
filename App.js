410c13170c92a296483cc7af620cccfd42a9eabd
christianbailon <xtian.bailon@gmail.com>
2020-06-19 05:15:00 +0800

    Changing State example 1

--- App.js
+++ App.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, {useState} from 'react';
 
+// ANCHOR Using class for state
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       answer: 'Yes',
+//     };
+//   }
 
+//   render() {
+//     return (
+//       <div>
+//         <h1>Is state important to know? {this.state.answer}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  // using as object
+  // const value = useState('Yes');
+  // using arraydestructing
+  const [answer] = useState('Yes');
+  console.log(answer);
+
+  return (
+    <div>
+      <h1>Is state important to know? {answer} </h1>
+    </div>
+  );
+}
 
 export default App;
