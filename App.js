c6497c2ebb2adabb8fc3408e2ce0f27551b36137
christianbailon <xtian.bailon@gmail.com>
2020-06-21 05:15:37 +0800

    Implementing useEffect() example1

--- App.js
+++ App.js
@@ -74,57 +74,93 @@
 // }
 
 // ANCHOR Using useState() example 3
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+
+// function App() {
+//   const [inputData, setInputData] = useState({ firstName: '', lastName: '' });
+//   const [contactsData, setContactsData] = useState([]);
+
+//   function handleChange(event) {
+// update our inputData state
+//   const { name, value } = event.target;
+//   setInputData(prevInputData => {
+//     return {
+//       ...prevInputData,
+//       [name]: value,
+//     };
+//   });
+// }
+
+// function handleSubmit(event) {
+// add the InputData to contactsData array
+// event.preventDefault();
+//     setContactsData(prevContacts => [...prevContacts, inputData]);
+//   }
+
+//   console.log(contactsData);
+
+//   const contacts = contactsData.map(contact => (
+//     <h2 key={contact.firstName + contact.lastName}>
+//       {contact.firstName} {contact.lastName}
+//     </h2>
+//   ));
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           placeholder="First Name"
+//           name="firstName"
+//           value={inputData.firstName}
+//           onChange={handleChange}
+//         />
+//         <input
+//           placeholder="Last Name"
+//           name="lastName"
+//           value={inputData.lastName}
+//           onChange={handleChange}
+//         />
+//         <br />
+//         <button>Add contact</button>
+//       </form>
+//       {contacts}
+//     </>
+//   );
+// }
+
+// ANCHOR Using useEffect() example 1
+// Lifecycle components that useEffect replaces are
+// componentDidMount
+// componentDidUpdate
+// componentWillUnmount
+
+// Side Effetcs?
+// Manual DOM manipulation
+// Event listeners or timeouts and intervals
+import React, { useState, useEffect } from 'react';
+import randomColor from 'randomcolor';
 
 function App() {
-  const [inputData, setInputData] = useState({ firstName: '', lastName: '' });
-  const [contactsData, setContactsData] = useState([]);
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('');
 
-  function handleChange(event) {
-    // update our inputData state
-    const { name, value } = event.target;
-    setInputData(prevInputData => {
-      return {
-        ...prevInputData,
-        [name]: value,
-      };
-    });
+  function increment() {
+    setCount(prevCount => prevCount + 1);
+  }
+  function decrement() {
+    setCount(prevCount => prevCount - 1);
   }
 
-  function handleSubmit(event) {
-    // add the InputData to contactsData array
-    event.preventDefault();
-    setContactsData(prevContacts => [...prevContacts, inputData]);
-  }
-
-  console.log(contactsData);
-
-  const contacts = contactsData.map(contact => (
-    <h2 key={contact.firstName + contact.lastName}>
-      {contact.firstName} {contact.lastName}
-    </h2>
-  ));
+  useEffect(() => {
+    setColor(randomColor())
+  },[count])
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="First Name"
-          name="firstName"
-          value={inputData.firstName}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="Last Name"
-          name="lastName"
-          value={inputData.lastName}
-          onChange={handleChange}
-        />
-        <br />
-        <button>Add contact</button>
-      </form>
-      {contacts}
-    </>
+    <div>      
+      <h1 style={{color: color}}>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
   );
 }
 
